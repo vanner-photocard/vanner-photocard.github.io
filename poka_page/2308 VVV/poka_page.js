acc64b6b4fedf6a022b50a9249adf4fb8540af5a
Minjung Hong <133033072+NyuNyuVVS@users.noreply.github.com>
2023-08-04 15:46:07 +0900

    fonts and more

--- poka_page/2308 VVV/poka_page.js
+++ poka_page/2308 VVV/poka_page.js
@@ -36,4 +36,22 @@
                x.style.display = "none";
            }
        }
+
+       $(function (){
+        // $('head').load("./header.html");
+        $(".headerDiv").load("./common.html #header");
+        $(".cardModeDiv").load("./common.html #cardModeButton");
+        // $(".cardSpan").load("./common.html #cards");
+       });
+
+
+
+       (function(d) {
+         var config = {
+           kitId: 'pfc1rmw',
+           scriptTimeout: 3000,
+           async: true
+         },
+         h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+       })(document);
    }
