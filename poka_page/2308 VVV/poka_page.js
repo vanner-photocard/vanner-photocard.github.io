e116099d3bd6b18c1fd497f34713b4e0ed52071f
Minjung Hong <133033072+NyuNyuVVS@users.noreply.github.com>
2023-08-20 15:24:51 +0900

    all done
    alll done

--- poka_page/2308 VVV/poka_page.js
+++ poka_page/2308 VVV/poka_page.js
@@ -1,57 +1,109 @@
-
 {
+    $(".headerDiv").load("./common.html #header");
+    $(".cardModeDiv").load("./common.html #cardModeButton");
+    cardModeChanger();
+    let cardMode = ""
+
+
     // 카드 상태 수정
-       let cardMode = ""
-       $("button").click(function () {
-           cardMode = ""
-           let id = this.id
-           cardMode = id
-           console.log(cardMode)
-       });
-
-       function changeImage(image) {
-           if (cardMode == "have") {
-               image.style.opacity = "1";
-           } else if (cardMode == "not_have") {
-               image.style.opacity = "0.5";
-           } else if (cardMode == "want") {
-               image.style.backgroundColor = "rgb(200,0,0)";
-               image.style.opacity = "0.8";
-           } else if (cardMode == "want_exchangeable") {
-               image.style.backgroundColor = "rgb(100,200,150)";
-               image.style.opacity = "1";
-           } else if (cardMode == "reset") {
-               image.style.backgroundColor = "rgb(255,255,255)"
-               image.style.opacity = "0.5";
-           }
-       }
-
-       function buttonHide() {
-           let x = document.getElementById("cardModeButton");
-
-           console.log(x.style.display + " yes")
-           if (x.style.display === "none") {
-               x.style.display = "block";
-           } else {
-               x.style.display = "none";
-           }
-       }
-
-       $(function (){
-        // $('head').load("./header.html");
-        $(".headerDiv").load("./common.html #header");
-        $(".cardModeDiv").load("./common.html #cardModeButton");
-        // $(".cardSpan").load("./common.html #cards");
-       });
+    function cardModeChanger() {
+        $("button").click(function () {
+            cardMode = ""
+            let id = this.id
+            cardMode = id
+            console.log(cardMode)
+        });
+    }
 
 
+    function changeImage(image) {
+        if (cardMode == "have") {
+            image.style.opacity = "1";
+        } else if (cardMode == "not_have") {
+            image.style.opacity = "0.5";
+        } else if (cardMode == "want") {
+            image.style.backgroundColor = "rgb(200,0,0)";
+            image.style.opacity = "0.8";
+        } else if (cardMode == "want_exchangeable") {
+            image.style.backgroundColor = "rgb(100,200,150)";
+            image.style.opacity = "1";
+        } else if (cardMode == "reset") {
+            window.location.reload();
+        }
+    }
 
-       (function(d) {
-         var config = {
-           kitId: 'pfc1rmw',
-           scriptTimeout: 3000,
-           async: true
-         },
-         h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-       })(document);
-   }
+    function buttonHide() {
+        let x = document.getElementById("cardModeButton");
+
+        console.log(x.style.display + " yes")
+        if (x.style.visibility === "visible") {
+            x.style.visibility = "hidden";
+        } else {
+            x.style.visibility = "visible";
+        }
+    }
+
+}
+
+function pageSetter(pageNameH3, pageNameH4){
+    $("#pageTitleH3").append(pageNameH3)
+    $("#pageTitleH4").append(pageNameH4)
+}
+
+
+
+
+function pokaAttacher(pokaArray, pokaName, imgFolderAdd, scnName) {
+
+    // 카드 폴더에서 업로드하기
+    let TAE = "1 TAE"
+    let GON = "2 GON"
+    let HYE = "3 HYE"
+    let AHX = "4 AHX"
+    let YGW = "5 YGW"
+    let ALL1 = "6 ALL"
+    let ALL2 = "7 ALL"
+    let MEM_ARR = [TAE, GON, HYE, AHX, YGW, ALL1, ALL2]
+    let POKA_NAME = pokaName;
+    let POKA_ARR = pokaArray;
+    let imgFolder = imgFolderAdd;
+    
+    let SCANNER_NAME = scnName;
+
+    //포카 넣기
+
+    for (let p = 0; p < POKA_ARR.length; p++) {
+
+        let pokaDivID = POKA_ARR[p];
+        let pokaName = POKA_NAME[p];
+        // ALL까지 몇개있는지 & MEM_ARR 조정
+        let pokaMember = parseInt(pokaDivID.substr(-2, 1))
+        if (pokaMember < 7) {
+            MEM_ARR = MEM_ARR.slice(0, pokaMember)
+        }
+        // 멤버별 몇종있는지
+        let pokaNumber = parseInt(pokaDivID.substr(-1, 1))
+        console.log(pokaDivID, pokaName, pokaMember, pokaNumber, MEM_ARR)
+
+        // table(카드판 cardPan)에 넣어주기
+        let default_html1 = `<tr><td><b class="pokaName">`
+        let default_html2 = `</b><div id=`
+        let default_html3 = ` class="pokaHolder"></div></td></tr>`
+        let basic_html = default_html1 + pokaName + default_html2 + pokaDivID + default_html3
+        $("#cardPan").append(basic_html)
+
+
+        for (let i = 1; i < pokaNumber + 1; i++) {
+            console.log(pokaNumber, i)
+
+            MEM_ARR.forEach((m) => {
+                img_html = `<img class="poka" src="${imgFolder}${pokaDivID} ${m} (${i}).jpg" onclick="changeImage(this)" onError="this.src='image/VPA WATERMARK_.png';">`
+                console.log(m, img_html)
+                $("#" + pokaDivID).append(img_html)
+            })
+        }
+    }
+    $("#scnName").append(SCANNER_NAME)
+
+}
+
